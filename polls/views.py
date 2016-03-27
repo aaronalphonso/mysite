ddb3b0b6f3d6e77473bc211a5aa933a284f159e4
aaronalphonso <alphonsoaaron1993@gmail.com>
2016-03-28 02:19:06 +0530

    tutorial 3 contd- using get_object_or_404, templates and removing hardcoded urls
    Modified details view to use get_object_or_404()
    Created template for details view.
    Modified index.html to remove hardcoded url
    Added app_name to urls.py and namespacing to the index.html template.

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from .models import Question
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 
 def index(request):
@@ -10,11 +10,9 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
+
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
